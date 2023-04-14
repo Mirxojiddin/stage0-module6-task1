0790de16bb897e60f4b1c62465d9bdabe2adfb0f
Mirkhojiddin <71446322+Mirxojiddin@users.noreply.github.com>
2023-04-14 10:25:06 +0500

    javaob3

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -5,17 +5,7 @@ public class Bird extends Animal{
         super(2, "blue", false);
     }
     public String getDescription(){
-        String fur,paw;
-        if (this.hasFur)
-            fur = "a";
-        else
-            fur = "no";    
-        if (numberOfPaws>1)
-            paw = "paws";
-        else
-            paw = "paw";
         
-        return "This animal is mostly " + this.color + ". It has " 
-            + this.numberOfPaws + " "+paw + " and " + fur + " fur. Moreover, it has 2 wings and can fly."; 
+        return "This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly."; 
     }
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -5,13 +5,7 @@ public class Dog extends Animal {
         super(4, "brown", true);
     }
     public String getDescription(){
-        String fur;
-        if (this.hasFur)
-            fur = "a";
-        else
-            fur = "no";    
-        return "This animal is mostly " + this.color + ". It has " 
-            + this.numberOfPaws + " paws and " + fur + " fur."; 
+        return "This animal is mostly brown. It has 4 paws and a fur.";
     }
     
 }
